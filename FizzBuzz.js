abc221a491c4c2857578ef1a26db06bc55b56e2e
Yonas Enchalew <88850442+Yonas1219@users.noreply.github.com>
2022-10-10 17:20:41 +0300

    Create FizzBuzz.js

---
+++ FizzBuzz.js
@@ -0,0 +1,15 @@
+var fizzBuzz = function(n) {
+    let output = [];
+    for(let i=1; i<=n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+    output.push("FizzBuzz");
+        }else if(i % 3 === 0){
+   output.push("Fizz");
+        }else if(i % 5 === 0){
+    output.push("Buzz");
+        }else{
+            output.push(i.toString());
+        }
+    }
+    return output;
+};
