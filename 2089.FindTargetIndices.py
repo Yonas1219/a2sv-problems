5ae76175f5b98f689b947237acd8469c76265f25
Yonas Enchalew <88850442+Yonas1219@users.noreply.github.com>
2022-10-11 10:06:09 +0300

    Create 2089.FindTargetIndices.py

---
+++ 2089.FindTargetIndices.py
@@ -0,0 +1,9 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        ans = []
+        
+        for i, x in enumerate(nums):
+            if x == target:
+                ans.append(i)
+        return ans
