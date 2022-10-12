dfe5e44359980239ae4b4d0a85c16b647f2eead5
Yonas Enchalew <88850442+Yonas1219@users.noreply.github.com>
2022-10-12 15:41:26 +0300

    Create 23.Merge-k-Sorted-Lists.py

---
+++ 23.Merge-k-Sorted-Lists.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # use the algorithm to sort two lists, from 21. merge two sorted lists
+    def mergeTwoLists(self, l1, l2):
+        answer = ListNode()
+        curr_node = answer
+        while (l1 and l2):
+            if l1.val < l2.val: curr_node.next, l1 = l1, l1.next
+            else: curr_node.next, l2 = l2, l2.next
+            curr_node = curr_node.next
+        curr_node.next = l2 if (l1 is None) else l1
+        return answer.next
+
+    def mergeKLists(self, lists: Optional[ListNode]) -> Optional[ListNode]:
+        # if the length of the array is less than zero
+        if len(lists) < 1: 
+            return None
+        
+        # just use merge sort
+        index = len(lists) - 1
+        while index > 0:
+            i, j = 0, index
+            while i < j:
+                lists[i] = self.mergeTwoLists(lists[i], lists[j])
+                i += 1
+                j -= 1
+                index = j
+
+        # return the last element in the list
+        return lists[0]
