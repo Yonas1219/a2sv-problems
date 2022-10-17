297eae0d6045a7739f0ff222826579ea59ed2fcc
Yonas Enchalew <88850442+Yonas1219@users.noreply.github.com>
2022-10-17 10:42:03 +0300

    Create 378.Kth-Smallest-Element-in-a-Sorted-Matrix.py

---
+++ 378.Kth-Smallest-Element-in-a-Sorted-Matrix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        l, r, N = matrix[0][0], matrix[-1][-1], len(matrix)
+        
+        def less_k(m):
+            cnt = 0
+            for r in range(N):
+                x = bisect_right(matrix[r], m)
+                cnt += x
+            return cnt
+        while l<r:
+            mid = (l+r)//2
+            if less_k(mid) < k:
+                l = mid + 1
+            else:
+                r = mid
+        return l
